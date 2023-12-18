5727ce854b7e22d50af71dfa3a0a4ace81c2743e
rzadp <roopert7@gmail.com>
2023-12-18 18:12:26 +0100

    extract toc constant

--- .github/scripts/gather-markdown-files.js
+++ .github/scripts/gather-markdown-files.js
@@ -4,12 +4,14 @@
 
 const fs = require('fs');
 
+const TOC = "**Table of Contents**\n\n<\!-- toc -->\n"
+
 module.exports = async ({github, context}) => {
     // Copy the approved (already-merged) RFCs markdown files, first adding a source link at the top and a TOC.
     for (const file of fs.readdirSync("text/")) {
       if (!file.endsWith(".md")) continue;
       const text = `[(source)](https://github.com/polkadot-fellows/RFCs/blob/main/text/${file})\n\n`
-        + "**Table of Contents**\n\n<\!-- toc -->\n"
+        + TOC
         + fs.readFileSync(`text/${file}`)
       fs.writeFileSync(`mdbook/src/approved/${file}`, text)
     }
@@ -19,7 +21,7 @@ module.exports = async ({github, context}) => {
       const dirPath = `${status}-rfcs/`
       for (const filename of fs.readdirSync(dirPath)) {
         if (!filename.endsWith(".md")) continue;
-        const text = "**Table of Contents**\n\n<\!-- toc -->\n"
+        const text = TOC
           + fs.readFileSync(dirPath + filename)
         // Source link is already there (with a link to PR). So we append here.
         fs.appendFileSync(`mdbook/src/${status}/${filename}`, text)
